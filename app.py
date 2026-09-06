f2ae96531a07b1d81521db83e256a62a5793f2de
waseemhashmidgkhan <abbass_waseem@outlook.com>
2026-09-06 21:44:18 +0500

    Update BASE_URL for Binance API

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime, timezone
 
-BASE_URL = "https://fapi.binance.com"
+BASE_URL = "https://data-api.binance.vision"
 S = requests.Session()
 S.headers.update({"User-Agent": "BinanceMarketScannerV2/2.0"})
 
